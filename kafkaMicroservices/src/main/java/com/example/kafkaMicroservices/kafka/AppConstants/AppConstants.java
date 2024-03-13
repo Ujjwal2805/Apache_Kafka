6ed4b46c9cb0764420df4231da718b1f7337aa58
ujjwal sengar <ujsengar@gmail.com>
2024-03-14 01:54:45 +0530

    Implemented Microservices producer/Consumer

---
+++ kafkaMicroservices/src/main/java/com/example/kafkaMicroservices/kafka/AppConstants/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.kafkaMicroservices.kafka.AppConstants;
+
+public class AppConstants {
+    public static final String TOPIC_NAME = "Wikipedia";
+    public static final String GROUP_ID = "group_id";
+}
